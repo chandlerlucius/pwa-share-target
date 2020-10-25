ca185be2562a2aeba7afb1af29ceeb29ee69d7b3
Chandler Lucius <clucius@chandlerlucius.com>
2020-10-25 18:17:34 -0500

    Update manifest and service worker for file share

--- service-worker.js
+++ service-worker.js
@@ -6,3 +6,16 @@ workbox.routing.registerRoute(
   new RegExp('/*'),
   new workbox.strategies.NetworkFirst()
 );
+
+self.addEventListener('fetch', event => {
+  if(event.request.method !== 'POST') {
+    return;
+  }
+
+  event.respondWith((async () => {
+    console.log(request);
+    const formData = await event.request.formData();
+    const image = formData.get('image');
+    console.log('image', image);
+  })());
+});
