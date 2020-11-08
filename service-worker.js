a2f5477f3f2769398ef21d03549025ed42ebf767
Chandler Lucius <clucius@chandlerlucius.com>
2020-11-08 11:27:32 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 self.skipWaiting();
 
 workbox.routing.registerRoute(
-  new RegExp('*'),
+  new RegExp('/*'),
   new workbox.strategies.NetworkFirst()
 );
 
