869bd789ef24b786be396d1fa397171c1bee1997
Chandler Lucius <clucius@chandlerlucius.com>
2022-01-16 12:35:35 -0600

    Skip waiting for service workers to install

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,13 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.2/workbox-sw.js');
 
-self.skipWaiting();
-
 workbox.routing.registerRoute(
   new RegExp('/*'),
   new workbox.strategies.NetworkFirst()
 );
+
+self.addEventListener('install', event => {
+  skipWaiting();
+});
 
 self.addEventListener('fetch', event => {
   if(event.request.method !== 'POST') {
