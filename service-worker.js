4296d85feba582ce2a4e338a9c86e67e58ce8c8e
Chandler Lucius <clucius@chandlerlucius.com>
2022-01-16 12:34:42 -0600

    Update workbox version to newest

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.2/workbox-sw.js');
 
 self.skipWaiting();
 
