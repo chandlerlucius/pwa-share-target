97ba214ac006e9db2ea08fc3fac590e57d49fa70
Chandler Lucius <clucius@chandlerlucius.com>
2022-01-16 18:18:28 -0600

    Removed old mapbox code

--- js/main.js
+++ js/main.js
@@ -33,7 +33,6 @@ const importFileAndParseExif = function (event) {
                 parseExifOtherData(this);
                 const latLng = parseExifGpsData(this);
                 reverseGeocodeGPS(latLng);
-                //initMap(latLng);
             });
         };
     };
@@ -117,102 +116,6 @@ const searchNearby = function (latLng) {
                      addToTable('', results[i].name);
                  }
             }
-            addToTable('hey', 'hey');
         }
     );
-    addToTable('no', 'no');
-}
-
-const initMap = function(latLng) {
-  const service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(
-    { location: latLng, radius: 100 },
-    (results, status, pagination) => {
-      if (status !== "OK") {
-          return;
-      }
-      createResults(results);
-      initMapbox(results);
-    }
-  );
-}
-
-const createResults = function(places) {
-  const placesList = document.getElementById("places");
-  for (let i = 0; i < 10; i++) {
-    const place = places[i];
-    const li = document.createElement("li");
-    li.textContent = place.name;
-    placesList.appendChild(li);
-  }
-}
-
-const initMapbox = function (places) {
-    mapboxgl.accessToken = 'pk.eyJ1IjoicnVubmluZ2hhaXIxOCIsImEiOiJjanN5dXF5aGgxNzVkNDNwcGI4NHp1bmp4In0.-YVFnu5m4LXZhZaHuZmQMQ';
-    const mapElement = 'map';
-    const map = new mapboxgl.Map({
-        container: mapElement,
-        style: 'mapbox://styles/mapbox/satellite-streets-v10?optimize=true',
-    });
-    map.addControl(new mapboxgl.NavigationControl());
-    createMapboxMarkersAndPopups(map, places);
-}
-
-const createMapboxMarkersAndPopups = function (map, places) {
-    const bounds = new mapboxgl.LngLatBounds();
-    places.forEach(function (place, index) {
-        const placeId = place.place_id;
-        const markerElement = document.createElement('img');
-
-        const popupElement = document.querySelector('.infowindow');
-        if (place.photos) {
-            const img = document.createElement('img');
-            img.alt = 'Photo of ' + place.name;
-            img.src = place.photos[0].getUrl({maxWidth: 35, maxHeight: 35});
-            img.style.filter = 'blur(10px)';
-
-            popupElement.querySelector('.card-image').prepend(img);
-            popupElement.querySelector('.card-image').style.height = '100%';
-            popupElement.style.height = '300px';
-        } else {
-            if (popupElement.querySelector('.card-image img') !== null) {
-                popupElement.querySelectorAll('.card-image img').forEach(function (img) {
-                    img.remove();
-                });
-            }
-            popupElement.querySelector('.card-image').style.height = '70%';
-            popupElement.style.height = '150px';
-        }
-        popupElement.querySelector('.card-title').innerHTML = place.name;
-        popupElement.querySelector('.card-name').innerHTML = place.name;
-        popupElement.querySelector('.card-country').innerHTML = place.country;
-
-        const popup = new mapboxgl.Popup({
-                anchor: 'left'
-            })
-            .setHTML(popupElement.outerHTML);
-
-        if (place.icon) {
-            markerElement.classList.add('marker');
-            markerElement.classList.add('marker-large');
-            markerElement.src = place.icon;
-        } else {
-            markerElement.classList.add('marker');
-            markerElement.classList.add('marker-small');
-            markerElement.src = 'data:image/jpeg' + index + ';base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=';
-        }
-        if (index === places.length - 1) {
-            markerElement.classList.add('marker-recent');
-        }
-        markerElement.alt = 'Thumbnail of ' + place.name;
-
-        const marker = new mapboxgl.Marker(markerElement)
-            .setLngLat([place.geometry.location.lng(), place.geometry.location.lat()])
-            .setPopup(popup)
-            .addTo(map);
-        bounds.extend(marker.getLngLat());
-    });
-    map.fitBounds(bounds, {
-        padding: 20
-    });
 }
